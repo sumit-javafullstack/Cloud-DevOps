f91e625bcc5d9c92e05d18c2b5f36e995da75451
unknown <mesumitkumarmandal@gmail.com>
2024-10-23 19:30:40 +0530

    sample java project

---
+++ src/main/java/com/aws/devops/aws_devops_project/AwsDevopsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aws.devops.aws_devops_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDevopsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDevopsProjectApplication.class, args);
+	}
+
+}
