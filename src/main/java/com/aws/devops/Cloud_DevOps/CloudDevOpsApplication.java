e9e56cd537179ff8b5899d1a7c3310f31c28d61f
unknown <mesumitkumarmandal@gmail.com>
2024-10-29 10:13:05 +0530

    Test

---
+++ src/main/java/com/aws/devops/Cloud_DevOps/CloudDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.aws.devops.Cloud_DevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/devops/Cloud_DevOps/CloudDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.devops.Cloud_DevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
